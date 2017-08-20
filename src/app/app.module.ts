b7059bdd5be5829ea67c25aa4077c693cda38cb3
Steve Witman <stevewitman@gmail.com>
2017-08-19 21:19:27 -0600

    Generate components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RequestComponent } from './inventory/request/request.component';
+import { ManageComponent } from './inventory/manage/manage.component';
+import { ApproveComponent } from './inventory/approve/approve.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RequestComponent,
+    ManageComponent,
+    ApproveComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
